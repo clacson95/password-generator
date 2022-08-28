529bfbadc913940ab201696fe436a82ad0215807
clacson95 <caleb.lacson@gmail.com>
2022-08-27 21:48:09 -0400

    swapped files out for starter code files

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// prompt
+
+let myCharacters = prompt("How many characters would you like your password to have?")
+
+
+
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
