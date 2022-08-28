6d95db693783023994382a235f88c9c70c3838b5
clacson95 <caleb.lacson@gmail.com>
2022-08-28 18:34:02 -0400

    added more variables, created function for uppercasin the alphabetcal characers,mgit ore work on the if statements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,27 @@
-//---------------
-//password values
-//---------------
 
-// special characters
-special = [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",",
+// Password values
+// Special characters
+const special = [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",",
 "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", 
 "`", "{", "|", "}", "~"];
 
-// numeric characters
-number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// Numeric characters
+const number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-// alphabetical characters
-alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+// Alphabetical characters
+const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
 "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+// UpperCase Alphabetical characters
+const alphabetCaps = alphabet.map(function (letter) {
+  return letter.toUpperCase();
+});
 
+// Variables
+var passLength;
 var generateBtn = document.querySelector("#generate");
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -23,18 +29,28 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 
+// Generate password
+function generatePassword() {
 
-// prompt
+  // Prompt user input
+  passLength = parseInt(prompt("How many characters would you like\
+  your password to have? Please enter a value between 8 and 128."));
 
-let myCharacters = prompt("How many characters would you like your password to have?")
+  if (passLength == null) {
+    alert("Please enter a value");
+  } else if (passLength < 8 || passLength > 128) {
+    passLength = parseInt(prompt("You must enter a value between 8 and 128"));
+  } else {
+    
+  }
 
 
 
-
+ 
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
